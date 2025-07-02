9c1a26f27703609bab420d1ed54590e8a943d65f
lucasOlivein <lucasolivein@gmail.com>
2025-07-02 04:49:41 -0300

    feat: solve puzzles 0, 1, 2, and 3

--- CS50IA/Project 1/knights/puzzle.py
+++ CS50IA/Project 1/knights/puzzle.py
@@ -12,21 +12,35 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Aknight or Aknave, but not both.
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+
+    Implication(AKnight, And(AKnight, AKnave)),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+    Implication(AKnight, And(AKnight, AKnave))
+
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+
+    Implication(AKnight, BKnight),
+    Implication(AKnave, BKnight),
+    Implication(BKnight, AKnave),
+    Implication(BKnave, AKnave)
 )
 
 # Puzzle 3
@@ -35,7 +49,22 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))),
+    
+
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnight, AKnave))),
+    Implication(BKnight, And(
+        CKnave,
+        Implication(AKnight, BKnave),
+        Implication(AKnave, BKnight)
+    )),
+    Implication(BKnave, CKnight),
+    Implication(CKnight, AKnight),
+    Implication(CKnave, AKnave),
+
 )
 
 
