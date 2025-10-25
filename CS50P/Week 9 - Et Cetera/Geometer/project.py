2970cc7d7fee44869edf02106dacf113df535fd1
lucasOlivein <lucasolivein@gmail.com>
2025-10-25 20:10:47 -0300

    chore: add back initial project scaffolding

---
+++ CS50P/Week 9 - Et Cetera/Geometer/project.py
@@ -0,0 +1,18 @@
+def main():
+    ...
+
+
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_3():
+    ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ CS50P/Week 9 - Et Cetera/Geometer/test_project.py
@@ -0,0 +1,15 @@
+from project import function_1
+from project import function_2
+from project import function_3
+
+
+def test_function_1():
+    ...
+
+
+def test_function_2():
+    ...
+
+
+def test_function_n():
+    ...
