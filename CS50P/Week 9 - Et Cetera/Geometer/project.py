378140a9c0e93d5d03745c4ab5b93f7aeb33b0f8
lucasOlivein <lucasolivein@gmail.com>
2025-10-26 17:53:44 -0300

    feat: implement "Render a Triangle" example from docs.pygfx.org

--- CS50P/Week 9 - Et Cetera/Geometer/project.py
+++ CS50P/Week 9 - Et Cetera/Geometer/project.py
@@ -1,5 +1,24 @@
+from rendercanvas.auto import RenderCanvas, loop
+import pygfx
+
+
+canvas = RenderCanvas()
+renderer = pygfx.WgpuRenderer(canvas)
+camera = pygfx.NDCCamera()
+
+triangle = pygfx.Mesh(
+    pygfx.Geometry(
+        indices=[(0, 1, 2)],
+        positions=[(0.0, -0.5, 0), (0.5, 0.5, 0), (-0.5, 1, 0)],
+        colors=[(1, 1, 0, 1), (1, 0, 1, 1), (0, 1, 1, 1)],
+    ),
+    pygfx.MeshBasicMaterial(color_mode="vertex"),
+)
+
 def main():
-    ...
+    # pygfx.show(triangle)
+    canvas.request_draw(lambda: renderer.render(triangle, camera))
+    loop.run()
 
 
 def function_1():
