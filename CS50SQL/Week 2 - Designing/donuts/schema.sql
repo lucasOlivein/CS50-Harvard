70c2fd245959f28df3453ffbd5b4cc2189e4cf4d
lucasOlivein <lucasOlivein@gmail.com>
2025-12-17 20:16:06 -0300

    feat(CS50SQL/week2/donuts): define donuts table

--- CS50SQL/Week 2 - Designing/donuts/schema.sql
+++ CS50SQL/Week 2 - Designing/donuts/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE "ingredients" (
     "price" REAL NOT NULL,
     "unit" TEXT NOT NULL
 );
+
+CREATE TABLE "donuts" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT,
+    "gluten-free" TEXT NOT NULL,
+    "price" REAL
+);
