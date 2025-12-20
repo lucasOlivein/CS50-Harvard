4f824b2407c2eab2383845e7e4c9b4a88604d0d1
lucasOlivein <lucasOlivein@gmail.com>
2025-12-20 13:43:31 -0300

    feat(CS50SQL/week5/dont-panic): remove password-update logs created by triggers

--- CS50SQL/Week 3 - Writing/dont-panic/hack.sql
+++ CS50SQL/Week 3 - Writing/dont-panic/hack.sql
@@ -11,3 +11,7 @@ WHERE "username" = 'admin';
 UPDATE "users"
 SET "password" = '982c0381c279d139fd221fce974916e7'
 WHERE "username" = 'admin';
+
+-- Remove the record of the last password update
+DELETE FROM "user_logs"
+WHERE "type" = 'update' AND "new_password" = '982c0381c279d139fd221fce974916e7';
