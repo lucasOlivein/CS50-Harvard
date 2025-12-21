6a2c2d50c2ff19b5aa1f97e765ff1d1f8388d545
lucasOlivein <lucasOlivein@gmail.com>
2025-12-21 20:32:57 -0300

    feat(CS50SQL/week3/meteorites): convert empty string values to NULL

--- CS50SQL/Week 3 - Writing/meteorites/import.sql
+++ CS50SQL/Week 3 - Writing/meteorites/import.sql
@@ -13,3 +13,21 @@ CREATE TABLE meteorites_temp (
 
 -- Load raw meteorite CSV data (skip header) into the staging table
 .import --csv --skip 1 meteorites.csv meteorites_temp
+
+-- Data cleaning: convert to NULL empty values
+-- Columns: mass, year, lat, long
+UPDATE "meteorites_temp"
+SET "mass" = NULL
+WHERE "mass" = '';
+
+UPDATE "meteorites_temp"
+SET "year" = NULL
+WHERE "year" = '';
+
+UPDATE "meteorites_temp"
+SET "lat" = NULL
+WHERE "lat" = '';
+
+UPDATE "meteorites_temp"
+SET "long" = NULL
+WHERE "long" = '';
